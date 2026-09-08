6a38957432223a9ddd08404d381fe9a3f425af14
Vicente Chemin Schmidt <vicentechemin@gmail.com>
2026-09-08 12:55:39 -0300

    Carrega Nunito localmente e unifica a fonte da página inicial

--- src/js/blog-teaser.js
+++ src/js/blog-teaser.js
@@ -1,6 +1,3 @@
-import "@fontsource/dm-sans/latin-400.css";
-import "@fontsource/dm-sans/latin-500.css";
-import "@fontsource/instrument-serif/latin-400.css";
 import "../css/blog-teaser.css";
 import { loadLatestPosts } from "./blog-teaser-view.cjs";
 
